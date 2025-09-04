94d4cae83608d32c5ce909e4bb7b6a0a10a8fe95
dakotafabro <dakotafabrocgu@gmail.com>
2025-09-04 14:22:16 -0700

    fix typing in loading dots component

--- components/LoadingDots.tsx
+++ components/LoadingDots.tsx
@@ -1,6 +1,10 @@
 "use client";
 import { ThreeDots } from "react-loader-spinner";
 
-export default function LoadingDots({ color }) {
-  return <ThreeDots height="18" width="48" color={color} ariaLabel="loading" />;
+type Props = { color?: string };
+
+export default function LoadingDots({
+  color = "var(--accent, #7aa2ff)",
+}: Props) {
+  return <ThreeDots height={18} width={48} color={color} ariaLabel="loading" />;
 }
